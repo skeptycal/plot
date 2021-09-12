8217e380181ae8ffffaec399912e509ffe8725d3
skeptycal <26148512+skeptycal@users.noreply.github.com>
2021-09-12 10:32:27 -0500

    GoBot: setup repo files from template

--- idea.md
+++ idea.md
@@ -1,4 +1,4 @@
-# gorepotemplate
+# plot
 
 > Tricky and fun utilities for Go programs on macOS.
 
---
+++ plot.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2021 Michael Treanor
+// https://github.com/skeptycal
+// MIT License
+
+// Package plot contains utilities for macOS.
+package plot
